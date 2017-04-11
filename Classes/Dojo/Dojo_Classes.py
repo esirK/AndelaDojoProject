79f670fad3a06f1fa57560e3a224a800c3e8302c
esirK <esirkings@gmail.com>
2017-04-11 12:04:32 +0300

    Remove Uneccessary Clases

--- Classes/Dojo/Dojo_Classes.py
+++ Classes/Dojo/Dojo_Classes.py
@@ -2,25 +2,3 @@
 class Dojo(Object):
     def __init__():
         pass
-
-class Room(Dojo):
-    def __init__():
-        pass
-
-class Office(Room):
-    def __init__():
-        pass
-
-class LivingSpace(Room):
-    def __init__(self):
-        pass
-class Person(Dojo):
-    def __init__(self):
-        pass
-class Staff(Person):
-    def __init__(self):
-        pass
-class Fellow(Person):
-    def __init__(self):
-        pass
-    
