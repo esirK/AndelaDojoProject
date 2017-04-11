8a81a0ebe82c9e3bb8537095a63f1093fc8029bd
esirK <esirkings@gmail.com>
2017-04-11 14:04:23 +0300

    Restructure Class Files

---
+++ Classes/Dojo/Person/test_person.py
@@ -0,0 +1,33 @@
+import unittest
+from person import Person,Fellow,Staff
+
+class TestPersons(unittest.TestCase):
+    #rooms
+    rooms=["Tsavo","ManEater","Makuti","Manyani"]
+    person=Person(rooms)
+    x_staff=Person(rooms)
+    x_fellow=Person(rooms)
+
+    def test_add_person(self):
+        num_persons=Person.num_of_persons_at_dojo
+        
+        self.x_staff.add_person("Mas","Staff")
+        self.x_fellow.add_person("Willi","Fellow","Y")
+        
+        num2_persons=Person.num_of_persons_at_dojo
+
+        self.assertEqual((num2_persons-num_persons),2)
+
+    def test_person_type_staff(self):
+        staff=self.x_staff.add_person("Mas","Staff")
+        #print (staff[0])
+        self.assertEqual(staff[0],"Staff")
+    
+    def test_person_type_fellow(self):
+        fellow=self.x_fellow.add_person("Willi","Fellow","Y")
+        #print (fellow[0])
+        self.assertEqual(fellow[0],"Fellow")
+    
+        
+if __name__=="__main__":
+    unittest.main()
