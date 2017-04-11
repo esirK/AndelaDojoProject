f64b0f029c10bd62fb83abe5e8308b6057f6eb4e
esirK <esirkings@gmail.com>
2017-04-11 04:23:26 +0300

    Add implementation for rooms creation

--- Classes/rooms/rooms.py
+++ Classes/rooms/rooms.py
@@ -9,10 +9,18 @@ class Room(object):
     def __init__(self):
         pass
 
-    def create_room(self,*args,what_type):
-        print (len(args))
+    def create_room(self,name,what_type):
         self.what_type=what_type
-        self.all_rooms+=1
+        asmany_names=[]
+        asmany_names.append(name)
+        if self.what_type=="LIVING_ROOM":
+            create_living_room=LivingSpace.create(self,name)
+            create_living_room
+        else:
+            creat_office=Office.create(self,name)
+            creat_office
+
+       # print (asmany_names)
         return self.all_rooms
 
 class Office(Room):
@@ -20,16 +28,34 @@ class Office(Room):
     size=6
     what_type="OFFICE"
     
-    def __init__(self):
+    def __init__(self,name):
+        #print ("Offiece"+name+" has Been successfully Created!")
         pass
     def create(self,name):
-        self.name=name
-
+        list_of_names=name.split(',')
+        #print (len(list_of_names))
+        for i in range(0,len(list_of_names)):
+            Room.all_rooms+=1
+            Room.rooms.append(list_of_names[i])
+            print ("Offiece "+list_of_names[i]+" has Been successfully Created!")
+        return Room.all_rooms
+        
 class LivingSpace(Room):
     size=4
     what_type="LIVING_ROOM"
-    def __init__(self):
+    def __init__(self,name):
+        #return " Living Room "+name+" has Been successfully Created!"
         pass
     def create(self,name):
-        self.name=name
+        list_of_names=name.split(',')
+        for i in range(0,len(list_of_names)):
+            Room.all_rooms+=1
+            Room.rooms.append(list_of_names[i])
+            print( "Living Room "+list_of_names[i]+" has Been successfully Created!")
+        return Room.all_rooms
 
+rx=Room()
+rx.create_room("Wabudabu,Joggoo","OFFICE")
+rl=Room()
+rl.create_room("Kahao,Morty","LIVING_ROOM")
+print(Room.rooms)
