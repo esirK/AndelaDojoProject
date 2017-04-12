ba4a20f42f85b9ae98a2b510a7a605dd0b585576
esirK <esirkings@gmail.com>
2017-04-12 13:58:14 +0300

    Add Method allocate_room

--- Classes/Dojo/Person/Staff/staff.py
+++ Classes/Dojo/Person/Staff/staff.py
@@ -1,3 +1,14 @@
-class Staff():
-    def __init__():
-        pass
+import sys
+import random
+#sys.path.append('../')
+import Person.person_new
+class Staff(Person.person_new.Person):
+    def __init__(self,person,rooms):
+        self.person=person
+        self.rooms=rooms
+    def allocate_room(self):
+        random_room_index=random.randrange(len(self.rooms))
+        print (self.person.name+" has been allocated the "+self.rooms[random_room_index].name)#
+        return self.rooms[random_room_index].append(self.person)#Add a Staff to the office
+
+        
