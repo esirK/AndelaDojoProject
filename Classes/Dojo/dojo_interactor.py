444aeb6672951ff4611abc3a41d8418086758d28
esirK <esirkings@gmail.com>
2017-04-13 09:23:01 +0300

    Update Files

--- Classes/Dojo/dojo_interactor.py
+++ Classes/Dojo/dojo_interactor.py
@@ -1,6 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 """
 Usage:
-    dojo create_room <room_name> <room_type>
+    dojo add_room <room_name> <room_type>
     dojo create_person <name> <person_type>[--Y | --N]
     dojo (-i | --interactive)
     dojo (-h | --help | --version)
@@ -14,24 +17,26 @@ import cmd
 from docopt import docopt, DocoptExit
 import Dojo_main
 
+
 def docopt_cmd(func):
     """
     This decorator is used to simplify the try/except block and pass the result
     of the docopt parsing to the called action.
     """
+
     def fn(self, arg):
         try:
             opt = docopt(fn.__doc__, arg)
-
         except DocoptExit as e:
+
             # The DocoptExit is thrown when the args do not match.
             # We print a message to the user and the usage block.
 
-            print('Invalid Command!')
-            print(e)
+            print ('Invalid Command!')
+            print (e)
             return
-
         except SystemExit:
+
             # The SystemExit exception prints the usage for --help
             # We do not need to do the print here.
 
@@ -45,33 +50,42 @@ def docopt_cmd(func):
     return fn
 
 
-class MyDojoInteracter (cmd.Cmd):
+class MyDojoInteracter(cmd.Cmd):
+
     intro = 'Welcome to Andela(Nairobi) Dojo!' \
         + ' (type help for a list of commands.)'
     prompt = '(**DOJO**) '
-    dojo=Dojo_main.Dojo()
+    dojo = Dojo_main.Dojo()
     file = None
 
     @docopt_cmd
     def do_create_room(self, arg):
-        """Usage: create_room <room_name> <room_type>"""
-        MyDojoInteracter.dojo.create_room(arg['<room_name>'],arg['<room_type>'])
-        #print(arg)
+        """Usage: add_room <room_name> <room_type>"""
+
+        MyDojoInteracter.dojo.create_room(arg['<room_name>'],
+                arg['<room_type>'])
+
+        # print(arg)
 
     @docopt_cmd
     def do_create_person(self, arg):
         """Usage: create_person <name> <person_type>[--Y | --N]"""
-        MyDojoInteracter.dojo.add_person(arg['<name>'],arg['<person_type>'],"Y")
+
+        MyDojoInteracter.dojo.add_person(arg['<name>'],
+                arg['<person_type>'], 'Y')
 
     def do_quit(self, arg):
         """Quits out of Interactive Mode."""
 
-        print('Good Bye!')
+        print ('Good Bye!')
         exit()
+
 
 opt = docopt(__doc__, sys.argv[1:])
 
 if opt['--interactive']:
     MyDojoInteracter().cmdloop()
 
-print(opt)
+print (opt)
+
+			
--- Classes/Dojo/test_dojo.py
+++ Classes/Dojo/test_dojo.py
@@ -1,24 +1,18 @@
 import unittest
-import Dojo_Classes,rooms.room_new
+import Dojo_main,rooms.room_new
 
 class TestDojo(unittest.TestCase):
-    #test Create rooms
-    def setUp(self):
-        self.test_room=rooms.room_new.Room("testRoom","OFFICE")
-        self.int_room=rooms.room_new.Room('wboo',"OFFICE")
+    #test Create room
     def test_create_room(self):
-        init_number_of_rooms_at_dojo=rooms.room_new.Room.rooms_at_dojo
-        new_room=rooms.room_new.Room("Wataf","OFFICE")
-        new_number_of_rooms_at_dojo=rooms.room_new.Room.rooms_at_dojo
-        self.assertEqual(new_number_of_rooms_at_dojo-init_number_of_rooms_at_dojo,1)
-   # def test_create_room_with_valid_names(self):
-       # with self.assertRaises(TypeError,msg="Allow string inputs only"):
-          #  self.int_room
-        #print(self.int_room.name)
+        init_number_of_rooms_at_dojo=len(Dojo_main.Dojo().list_office_rooms)
+        new_room=Dojo_main.Dojo().add_room("Wataf","OFFICE")
+        new_number_of_rooms_at_dojo=len(Dojo_main.Dojo().list_office_rooms)
+        self.assertEqual(new_number_of_rooms_at_dojo-init_number_of_rooms_at_dojo,0)
     def test_case_for_wrong_room_type(self):
         self.assertTrue(rooms.room_new.Room("Gidp","OFFICE").valid_room_type,msg="Wrong Room Type")
         
     def test_case_create_room_with_no_name(self):
         self.assertEqual(rooms.room_new.Room(" ","OFFICE").nyce_name,"Empty Room Name",msg="No room Name Provided")
+
 if __name__=="__main__":
     unittest.main()
